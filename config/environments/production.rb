fa5f809cc6272fcaebbf6ce4e7c20608cad5ecd2
denis <mentalitapp@gmail.com>
2026-02-19 21:50:30 -0500

    Added capacitor enable mobile sessions

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,9 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.session_store :cookie_store,
+  key: '_pax_measurer_session',
+  same_site: :none,
+  secure: true
 end
